03ee27610600e5d696239a0516a34fd9326acbf2
Scott Morgan <scott@adligo.com>
2021-12-24 16:50:03 -0600

    This is working with or with out the -Ptag cool

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+plugins {
+  `maven-publish`
+  eclipse
+  java
+  signing
+}
+
+dependencies {
+  implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+  implementation("org.mockito:mockito-all:2.0.2-beta")
+  implementation("org.adligo:tests4j:vg0_7")
+}
+
+sourceSets {
+  main {
+    java {
+      srcDirs("src")
+    }
+  }
+}
+
+fun getProp(key: String, default: String): String {
+  var r : String = default
+  if (project.hasProperty(key)) {
+    r = project.getProperties().get(key) as String
+  }
+  return r
+}
+
+fun getTag(): String {
+  val current = LocalDateTime.now()
+  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")
+  val snapshot = "SNAPSHOT_" + current.format(formatter)
+  //println(snapshot)
+  return getProp("tag", snapshot)
+}
+
+// gradle publishToMavenLocal 
+// or with the git tag
+//  gradle publishToMavenLocal -Ptag=vg0_1 
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      groupId = "org.adligo"
+      artifactId = "tests4j_4mockito"
+      version = getTag()
+      from(components["java"])
+    }
+  }
+}
+
+
+repositories {
+  mavenLocal()
+  mavenCentral()
+}
